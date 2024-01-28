6abd0094ef525a260055abbf0367829a50a3be2c
tylerbert31 <tbaring_ccs@uspf.edu.ph>
2024-01-28 17:12:46 +0800

    Enhancements: Endpoint

--- src/lib/endpoints.js
+++ src/lib/endpoints.js
@@ -1,7 +1,9 @@
 const endpoints = {
-  local: process.env.API_ENDPOINT,
+  local: process.env.API_LOCAL,
+  production: process.env.API_ENDPOINT,
 };
 
 const endpoint = endpoints.local;
+console.log(endpoint);
 
 export { endpoint };
